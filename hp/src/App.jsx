42a54ca0db81af4f9c3c129f6769aa63072d6ba0
ngolodets <nina.golodets@gmail.com>
2020-02-06 12:47:05 -0800

    playing with visibility toggle

--- hp/src/App.jsx
+++ hp/src/App.jsx
@@ -3,12 +3,14 @@ import axios from 'axios';
 import Students from './Students';
 import Staff from './Staff';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, withRouter, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   const [allCharacters, setAllCharacters] = useState([]);
   const [house, setHouse] = useState('');
   const [houses, setHouses] = useState([]);
+  const [isHidden, setIsHidden] = useState(true);
+  //const [character, setCharacter] = useState([]);
 
   useEffect(function() {
     let url = 'http://hp-api.herokuapp.com/api/characters';
@@ -53,31 +55,43 @@ function App() {
     setHouse(e.target.value);  
   }
 
+  function showCharInfo() {
+    if (isHidden === true) {
+      setIsHidden(false);
+    } else {
+      setIsHidden(true);
+    }
+  }
+
   let content;
   if (allCharacters.length > 0) {
     content = allCharacters.map(function(character, index) {
       let wand = character.wand;
       
       return (
-        <div key={index}>
-          <h3>{character.name}</h3>
-          <p>Played by {character.actor}</p>
+        <div key={index} className='characters' onClick={showCharInfo}>
+          <h3 className='charName'>{character.name}</h3>
           <img src={character.image} 
                 alt={character.name}
                 id='char-pic'
-          />  
-          <div style={{display: wand.wood ? 'block' : 'none'}}>
-            <h4>Wand Information</h4>
-            <p>Material: {wand.wood}</p>
-            <p>Core: {wand.core}</p>
-            <p>Length: {wand.length} inches</p>
-          </div>
+          /> 
+          {isHidden && 
+          <div>
+            <p className='actor'>Played by {character.actor}</p>
+            <div style={{display: wand.wood ? 'block' : 'none'}}>
+              <h4>Wand Information</h4>
+              <p>Material: {wand.wood}</p>
+              <p>Core: {wand.core}</p>
+              <p>Length: {wand.length} inches</p>
+            </div>
+          </div>}
         </div>
       )
     })
   } else {
     content = <h1>There Are No Characters!</h1>
   }
+
   return (
     <Router>
       <div className="App">
@@ -128,6 +142,7 @@ function App() {
             </label>
           </form>
         </nav>
+        
         <Switch>
           <Route exact path='/students' component={Students} />
           <Route exact path='/staff' component={Staff} />
